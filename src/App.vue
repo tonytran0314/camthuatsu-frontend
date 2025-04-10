ef85817bea42a3e7484583446ccdaf77b7de4713
Trần Gia Huy <tonytran0314@gmail.com>
2025-04-10 02:30:11 -0400

    install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref, onMounted } from 'vue'
+
+  const soundData = ref([])
+
+  onMounted(async () => {
+    try {
+      const response = await fetch('/sounds.json')
+      const data = await response.json()
+      soundData.value = data
+    } catch (error) {
+      console.error('Error fetching JSON data:', error)
+    }
+  })
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Danh sách nhạc</h1>
+    <ul>
+      <li v-for="(sound, index) in soundData" :key="index" class="">
+        {{ sound.name }}
+        <br>
+        <video :src="sound.url" controls></video>
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
