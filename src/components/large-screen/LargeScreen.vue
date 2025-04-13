c0ec2a036908fea81a066ab784530e4a5342042b
Trần Gia Huy <tonytran0314@gmail.com>
2025-04-13 02:40:20 -0400

    fix responsive

--- src/components/large-screen/LargeScreen.vue
+++ src/components/large-screen/LargeScreen.vue
@@ -11,49 +11,58 @@
 <template>
     <div class="flex w-full h-screen bg-slate-900">
         <!-- left col -->
-        <div class="h-screen pb-12 w-[60%] overflow-x-hidden overflow-y-auto scrollbar-hide">
+        <div class="flex-1 h-screen pb-12 overflow-x-hidden overflow-y-auto scrollbar-hide" :class="songStore.currentSong.id ? 'w-[60%]' : 'w-[100%]'">
         <!-- header -->
             <div class="w-full h-16 flex justify-center items-center">
                 <p class="font-playwrite text-slate-100 text-2xl">Cấm Thuật Sư</p>
             </div>
 
             <!-- songs list -->
-            <div class="w-full flex flex-wrap justify-center items-start gap-x-6 gap-y-8 py-8 pl-8 pr-4">
+            <div class="w-full flex flex-wrap justify-center items-start gap-x-6 gap-y-8 py-8 pl-8" :class="songStore.currentSong.id ? 'pr-4' : 'pr-8'">
                 <SongItem v-for="(song, index) in songStore.songs" @click="songStore.setCurrentSong(song)" :imageFile="song.imageFile" :title="song.title" />
             </div>
         </div>
 
         <!-- right col -->
-        <div class="h-screen w-[40%] p-8 text-slate-100">
-        <!-- current song card -->
-        <div v-if="songStore.currentSong.title || songStore.currentSong.imageFile" class="w-full rounded-2xl px-16 pt-16 pb-12 bg-slate-800 flex flex-col gap-8">
-            <div class="flex flex-col gap-4">
-                <div>
-                    <img :src="songStore.currentSong.imageFile" class="w-full aspect-square rounded-lg" alt="song cover">
+        <div v-if="songStore.currentSong.id" class="h-screen w-auto p-8 text-slate-100 flex items-center">
+            <!-- current song card -->
+            <div v-if="songStore.currentSong.title || songStore.currentSong.imageFile" class="w-full rounded-2xl flex flex-col items-center">
+                <div class="flex flex-col gap-4 max-w-lg">
+                    <div class="flex justify-center">
+                        <img :src="songStore.currentSong.imageFile" class="w-full max-w-lg aspect-square rounded-lg" alt="song cover">
+                    </div>
+                    <p class="text-slate-100 text-2xl line-clamp-1 px-6 pt-6 bg-slate-800 rounded-t-2xl">{{ songStore.currentSong.title }}</p>
                 </div>
-                <p class="text-slate-100 text-2xl line-clamp-1">{{ songStore.currentSong.title }}</p>
-            </div>
-            <div class="w-full h-auto flex justify-center">
-                <div class="flex justify-around items-center w-full">
-                    <i class="fa-solid fa-shuffle text-xl text-slate-500 cursor-pointer"></i>
-                    <div class="flex items-center gap-8">
-                        <i class="fa-solid fa-backward-step text-4xl cursor-pointer text-slate-100"></i>
-                        <div class="bg-slate-800 size-20 rounded-full flex justify-center items-center">
-                            <div @click="songStore.toggle()">
-                                <i v-if="songStore.isPlaying" class="fa-solid fa-pause text-5xl cursor-pointer"></i>
-                                <i v-else class="fa-solid fa-play text-5xl cursor-pointer"></i>
+                <div class="w-full max-w-lg h-auto flex justify-center bg-slate-800 pt-8 pb-6 rounded-b-2xl">
+                    <div class="flex flex-col xl:flex-row justify-around 2xl:justify-center 2xl:gap-12 items-center w-full gap-y-8">
+                        <div class="hidden xl:block">
+                            <i class="fa-solid fa-shuffle text-xl text-slate-500 cursor-pointer"></i>
+                        </div>
+                        <div class="flex items-center gap-8">
+                            <i class="fa-solid fa-backward-step text-2xl cursor-pointer text-slate-100"></i>
+                            <div class="bg-slate-600 size-14 xl:size-16 rounded-full flex justify-center items-center">
+                                <div @click="songStore.toggle()">
+                                    <i v-if="songStore.isPlaying" class="fa-solid fa-pause text-3xl xl:text-4xl cursor-pointer"></i>
+                                    <i v-else class="fa-solid fa-play text-3xl xl:text-4xl cursor-pointer"></i>
+                                </div>
+                            </div>
+                            <i class="fa-solid fa-forward-step text-2xl cursor-pointer text-slate-100"></i>
+                        </div>
+                        
+                        <div @click="songStore.toggleRepeat" class="hidden xl:block">
+                            <i v-if="songStore.repeatCurrentSong" class="fa-solid fa-repeat text-xl text-slate-100 cursor-pointer"></i>
+                            <i v-else class="fa-solid fa-repeat text-xl text-slate-500 cursor-pointer"></i>
+                        </div>
+                        <div class="flex items-center w-full justify-center gap-12 xl:hidden">
+                            <i class="fa-solid fa-shuffle text-xl text-slate-500 cursor-pointer"></i>
+                            <div @click="songStore.toggleRepeat">
+                                <i v-if="songStore.repeatCurrentSong" class="fa-solid fa-repeat text-xl text-slate-100 cursor-pointer"></i>
+                                <i v-else class="fa-solid fa-repeat text-xl text-slate-500 cursor-pointer"></i>
                             </div>
                         </div>
-                        <i class="fa-solid fa-forward-step text-4xl cursor-pointer text-slate-100"></i>
-                    </div>
-                    
-                    <div @click="songStore.toggleRepeat">
-                        <i v-if="songStore.repeatCurrentSong" class="fa-solid fa-repeat text-xl text-slate-100 cursor-pointer"></i>
-                        <i v-else class="fa-solid fa-repeat text-xl text-slate-500 cursor-pointer"></i>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     </div>
 </template>
--- src/components/small-screen/FullScreenCurrentSong.vue
+++ src/components/small-screen/FullScreenCurrentSong.vue
@@ -7,33 +7,42 @@
 </script>
 
 <template>
-    <div class="w-full h-screen bg-slate-900 flex flex-col">
-        <div class="w-full h-12 flex items-center justify-end p-8 sm:p-16">
-            <i @click="fullScreenSongStore.exitFullscreen" class="fa-solid fa-down-left-and-up-right-to-center text-slate-100 text-xl sm:text-2xl md:text-3xl p-1 sm:p-2 md:p-3 cursor-pointer"></i>
+    <div class="w-full h-screen bg-slate-900 flex flex-col overflow-y-auto pb-16">
+        <div class="w-full h-12 flex items-center justify-end p-8">
+            <i @click="fullScreenSongStore.exitFullscreen" class="fa-solid fa-down-left-and-up-right-to-center text-slate-100 text-xl sm:text-2xl p-1 cursor-pointer"></i>
         </div>
         <div class="w-full flex flex-col flex-1 px-12 justify-center gap-16">
-            <div class="flex flex-col gap-6">
-                <div>
-                    <img :src="songStore.currentSong.imageFile" class="w-full aspect-square rounded-lg" alt="song cover">
+            <div class="flex flex-col gap-6 items-center">
+                <div class="max-w-2xl w-full">
+                    <img :src="songStore.currentSong.imageFile" class="w-full max-w-2xl aspect-square rounded-lg" alt="song cover">
                 </div>
                 <p class="text-slate-100 text-2xl line-clamp-1">{{ songStore.currentSong.title }}</p>
             </div>
             <div class="w-full h-auto flex justify-center">
-                <div class="flex justify-around items-center w-full">
-                    <i class="fa-solid fa-shuffle text-xl text-slate-500 cursor-pointer"></i>
-                    <div class="flex items-center gap-8">
-                        <i class="fa-solid fa-backward-step text-4xl cursor-pointer text-slate-100"></i>
-                        <div @click="songStore.toggle()" class="bg-slate-800 size-20 rounded-full flex justify-center items-center cursor-pointer">
+                <div class="flex flex-col sm:flex-row sm:justify-center sm:gap-12 gap-y-8 justify-around items-center w-full max-w-2xl">
+                    <div class="hidden sm:block">
+                        <i class="fa-solid fa-shuffle text-xl text-slate-500 cursor-pointer"></i>
+                    </div>
+                    <div class="flex items-center gap-6">
+                        <i class="fa-solid fa-backward-step text-2xl cursor-pointer text-slate-100"></i>
+                        <div @click="songStore.toggle()" class="bg-slate-800 size-16 rounded-full flex justify-center items-center cursor-pointer">
                             <div>
-                                <i v-if="songStore.isPlaying" class="fa-solid fa-pause text-4xl text-slate-100 cursor-pointer"></i>
-                                <i v-else class="fa-solid fa-play text-4xl text-slate-100 cursor-pointer"></i>
+                                <i v-if="songStore.isPlaying" class="fa-solid fa-pause text-2xl text-slate-100 cursor-pointer"></i>
+                                <i v-else class="fa-solid fa-play text-2xl text-slate-100 cursor-pointer"></i>
                             </div>
                         </div>
-                        <i class="fa-solid fa-forward-step text-4xl cursor-pointer text-slate-100"></i>
+                        <i class="fa-solid fa-forward-step text-2xl cursor-pointer text-slate-100"></i>
                     </div>
-                    <div @click="songStore.toggleRepeat">
+                    <div @click="songStore.toggleRepeat" class="hidden sm:block">
                         <i v-if="songStore.repeatCurrentSong" class="fa-solid fa-repeat text-xl text-slate-100 cursor-pointer"></i>
                         <i v-else class="fa-solid fa-repeat text-xl text-slate-500 cursor-pointer"></i>
+                    </div>
+                    <div class="flex items-center w-full justify-center gap-12 sm:hidden">
+                        <i class="fa-solid fa-shuffle text-xl text-slate-500 cursor-pointer"></i>
+                        <div @click="songStore.toggleRepeat">
+                            <i v-if="songStore.repeatCurrentSong" class="fa-solid fa-repeat text-xl text-slate-100 cursor-pointer"></i>
+                            <i v-else class="fa-solid fa-repeat text-xl text-slate-500 cursor-pointer"></i>
+                        </div>
                     </div>
                 </div>
             </div>
